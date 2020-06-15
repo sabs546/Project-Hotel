6fa4b1fae9929256463c5bdb9ce8547ebba766cf
sabs546 <55698170+sabs546@users.noreply.github.com>
2020-06-15 23:19:20 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        { // Up
+            transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        { // Down
+            transform.Translate(0.0f, -speed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        { // Left
+            transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        { // Right
+            transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+
+    private void Collider()
+    {
+
+    }
+}
