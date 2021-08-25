b8786e24bf29de74997f63b2958f4fc42f8fb79a
sabs546 <55698170+sabs546@users.noreply.github.com>
2021-08-25 22:29:39 +0100

    Camera update
    Camera unlocking where it follows to a certain value for larger rooms, new outside starting area with prototype room made

---
+++ Assets/Scripts/AI/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public bool unlockX;
+    public bool unlockY;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unlockY && player.position.y > minY && player.position.y < maxY)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/AI/CameraControlEditor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraControl))]
+public class CameraControlEditor : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        CameraControl script = target as CameraControl;
+
+        script.unlockX = EditorGUILayout.Toggle("Unlock X", script.unlockX);
+
+        using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(script.unlockX)))
+        {
+            if (group.visible)
+            {
+                script.minX = EditorGUILayout.FloatField("Left", script.minX);
+                script.maxX = EditorGUILayout.FloatField("Right", script.maxX);
+            }
+        }
+
+        script.unlockY = EditorGUILayout.Toggle("Unlock Y", script.unlockY);
+
+        using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(script.unlockY)))
+        {
+            if (group.visible)
+            {
+                script.minY = EditorGUILayout.FloatField("Bottom", script.minY);
+                script.maxY = EditorGUILayout.FloatField("Top", script.maxY);
+            }
+        }
+    }
+}
